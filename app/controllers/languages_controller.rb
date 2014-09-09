74eeed578b0642191dc313d7bf0d95d101a388c2
raghunayyar <me@iraghu.com>
2014-09-09 16:06:01 +0530

    Sample: Implements Static JSON response for one language.

--- app/controllers/languages_controller.rb
+++ app/controllers/languages_controller.rb
@@ -6,7 +6,27 @@ class LanguagesController < ApplicationController
   end
 
   def enabled
-  	@data = CSV.read("uploads/languages/malayalamLayout.csv")
-  	render json: @data
+    # @data = CSV.read("uploads/languages/malayalamLayout.csv")
+    my_json = {
+      :id => "32",
+      :name => "hindi",
+      :csv => [
+        {
+           :keycode => "1",
+            :unicode => "u0D15",
+            :chakra => "1"
+        },{
+           :keycode => "1",
+            :unicode => "u0D15",
+            :chakra => "1"
+        },{
+           :keycode => "1",
+            :unicode => "u0D15",
+            :chakra => "1"
+        }
+      ]
+    }
+    @data = JSON.pretty_generate(my_json)
+    render json: @data
   end
 end
