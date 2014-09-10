08be791e5974d275134aceec4c1b42c624fefc09
raghunayyar <me@iraghu.com>
2014-09-10 15:36:52 +0530

    Enhancement: Returns correct JSON for one file.

--- app/controllers/languages_controller.rb
+++ app/controllers/languages_controller.rb
@@ -1,26 +1,43 @@
 class LanguagesController < ApplicationController
+  
   def index
   end
 
   def all
+    alllanguages = {
+      :id => "32",
+      :name => "hindi",
+      :csv => []
+    }
+
+    CSV.foreach("uploads/languages/malayalamLayout.csv") do |row|
+      alllanguages[:csv] << {
+        :keycode => row[0],
+        :unicode => row[1],
+        :chakra => row[2]
+      }
+    end
+    @data = JSON.pretty_generate(alllanguages)
+    puts @data
+    render json: @data 
   end
 
   def enabled
     enabledlanguages = {
       :id => "32",
-      :name => "hindi"
+      :name => "hindi",
+      :csv => []
     }
+
     CSV.foreach("uploads/languages/malayalamLayout.csv") do |row|
-      enabledlanguages[:csv] = [
-        {
-          :keycode => row[0],
-          :unicode => row[1],
-          :chakra => row[2]
-        }
-      ]
-      puts enabledlanguages[:csv]
+      enabledlanguages[:csv] << {
+        :keycode => row[0],
+        :unicode => row[1],
+        :chakra => row[2]
+      }
     end
     @data = JSON.pretty_generate(enabledlanguages)
-    render json: @data
+    puts @data
+    render json: @data 
   end
 end
