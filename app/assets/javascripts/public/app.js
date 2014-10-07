5952b96df898ae83c3ffe4d66d95b9f6588c80c5
raghunayyar <me@iraghu.com>
2014-10-07 15:18:42 +0530

    Enhancement: Implements Dynamic Calling, Cheers to Georg.

--- app/assets/javascripts/public/app.js
+++ app/assets/javascripts/public/app.js
@@ -22,6 +22,12 @@
     }
   ]);
 
+  app.filter("unsafeFilter", function($sce) {
+    return function(val) {
+      return $sce.trustAsHtml(val);
+    };
+  });
+
   app.controller("AppController", [
     "$scope", "Restangular", "LanguageModel", function($scope, Restangular, LanguageModel) {
       var languageResource;
@@ -60,347 +66,45 @@
   app.controller("KeyboardController", [
     "$scope", "Restangular", "LanguageModel", "$sce", function($scope, Restangular, LanguageModel, $sce) {
       var languageResource;
+      languageResource = void 0;
       $scope.languages = LanguageModel.getAll();
       languageResource = Restangular.one("languages");
-      angular.element('#chakra').css('display', 'none');
+      angular.element("#chakra").css("display", "none");
       languageResource.getList("all").then(function(languageobject) {
-        var i, j, k, totaljuniorrarray;
+        var div, grouped, i, j, k, mod, totaljuniorrarray;
+        i = void 0;
+        j = void 0;
+        k = void 0;
+        totaljuniorrarray = void 0;
         LanguageModel.addAll(languageobject);
         $scope.currentlanguage = $scope.languages[0].name;
-        totaljuniorrarray = Math.ceil($scope.languages[0].csv.length / 5);
-        $scope.masterarray = [];
-        k =0;
-        for (i=0; i<totaljuniorrarray; i++) {
-          $scope.juniorarray = new Array(5);
-          for (j=k; j<$scope.languages[0].csv.length; j++) {
-             if (j%5 != 0) {
-              $scope.juniorarray[j] = $scope.languages[0].csv[j];
-             } else if (j=0) {
-              continue;
-             } else {
-              k = j
-              break;
-             }
-             $scope.masterarray[i] = $scope.juniorarray;
+        $scope.totalrows = $scope.languages[0].csv.length;
+        grouped = [];
+        i = 0;
+        while (i < $scope.totalrows) {
+          div = Math.floor(i / 5);
+          mod = i % 5;
+          if (mod === 0) {
+            grouped[div] = [];
           }
+          grouped[div][mod] = $scope.languages[0].csv[i];
+          i++;
         }
-        $scope.onscreen = [
-          [
-            {
-              keycode: "1",
-              unicode: "\u0D15",
-              chakra: "1"
-            }, {
-              keycode: "2",
-              unicode: "\u0D16",
-              chakra: "1"
-            }, {
-              keycode: "3",
-              unicode: "\u0D17",
-              chakra: "1"
-            }, {
-              keycode: "4",
-              unicode: "\u0D18",
-              chakra: "1"
-            }, {
-              keycode: "5",
-              unicode: "\u0D19",
-              chakra: "1"
-            }
-          ], [
-            {
-              keycode: "6",
-              unicode: "\u0D1A",
-              chakra: "1"
-            }, {
-              keycode: "7",
-              unicode: "\u0D1B",
-              chakra: "1"
-            }, {
-              keycode: "8",
-              unicode: "\u0D1C",
-              chakra: "1"
-            }, {
-              keycode: "9",
-              unicode: "\u0D1D",
-              chakra: "1"
-            }, {
-              keycode: "10",
-              unicode: "\u0D1E",
-              chakra: "1"
-            }
-          ], [
-            {
-              keycode: "11",
-              unicode: "\u0D15",
-              chakra: "1"
-            }, {
-              keycode: "12",
-              unicode: "\u0D16",
-              chakra: "1"
-            }, {
-              keycode: "13",
-              unicode: "\u0D17",
-              chakra: "1"
-            }, {
-              keycode: "14",
-              unicode: "\u0D18",
-              chakra: "1"
-            }, {
-              keycode: "15",
-              unicode: "\u0D19",
-              chakra: "1"
-            }
-          ], [
-            {
-              keycode: "16",
-              unicode: "\u0D1A",
-              chakra: "1"
-            }, {
-              keycode: "17",
-              unicode: "\u0D1B",
-              chakra: "1"
-            }, {
-              keycode: "18",
-              unicode: "\u0D1C",
-              chakra: "1"
-            }, {
-              keycode: "19",
-              unicode: "\u0D1D",
-              chakra: "1"
-            }, {
-              keycode: "20",
-              unicode: "\u0D1E",
-              chakra: "1"
-            }
-          ], [
-            {
-              keycode: "21",
-              unicode: "\u0D15",
-              chakra: "1"
-            }, {
-              keycode: "22",
-              unicode: "\u0D16",
-              chakra: "1"
-            }, {
-              keycode: "23",
-              unicode: "\u0D17",
-              chakra: "1"
-            }, {
-              keycode: "24",
-              unicode: "\u0D18",
-              chakra: "1"
-            }, {
-              keycode: "25",
-              unicode: "\u0D19",
-              chakra: "1"
-            }
-          ], [
-            {
-              keycode: "26",
-              unicode: "\u0D15",
-              chakra: "1"
-            }, {
-              keycode: "27",
-              unicode: "\u0D16",
-              chakra: "1"
-            }, {
-              keycode: "28",
-              unicode: "\u0D17",
-              chakra: "1"
-            }, {
-              keycode: "29",
-              unicode: "\u0D18",
-              chakra: "1"
-            }, {
-              keycode: "30",
-              unicode: "\u0D19",
-              chakra: "1"
-            }
-          ], [
-            {
-              keycode: "31",
-              unicode: "\u0D1A",
-              chakra: "1"
-            }, {
-              keycode: "32",
-              unicode: "\u0D1B",
-              chakra: "1"
-            }, {
-              keycode: "33",
-              unicode: "\u0D1C",
-              chakra: "1"
-            }, {
-              keycode: "34",
-              unicode: "\u0D1D",
-              chakra: "1"
-            }, {
-              keycode: "35",
-              unicode: "\u0D1E",
-              chakra: "1"
-            }
-          ], [
-            {
-              keycode: "36",
-              unicode: "\u0D15",
-              chakra: "1"
-            }, {
-              keycode: "37",
-              unicode: "\u0D16",
-              chakra: "1"
-            }, {
-              keycode: "38",
-              unicode: "\u0D17",
-              chakra: "1"
-            }, {
-              keycode: "39",
-              unicode: "\u0D18",
-              chakra: "1"
-            }, {
-              keycode: "40",
-              unicode: "\u0D19",
-              chakra: "1"
-            }
-          ], [
-            {
-              keycode: "41",
-              unicode: "\u0D1A",
-              chakra: "1"
-            }, {
-              keycode: "42",
-              unicode: "\u0D1B",
-              chakra: "1"
-            }, {
-              keycode: "43",
-              unicode: "\u0D1C",
-              chakra: "1"
-            }, {
-              keycode: "44",
-              unicode: "\u0D1D",
-              chakra: "1"
-            }, {
-              keycode: "45",
-              unicode: "\u0D1E",
-              chakra: "1"
-            }
-          ], [
-            {
-              keycode: "46",
-              unicode: "\u0D15",
-              chakra: "1"
-            }, {
-              keycode: "47",
-              unicode: "\u0D16",
-              chakra: "1"
-            }, {
-              keycode: "48",
-              unicode: "\u0D17",
-              chakra: "1"
-            }, {
-              keycode: "49",
-              unicode: "\u0D18",
-              chakra: "1"
-            }, {
-              keycode: "50",
-              unicode: "\u0D19",
-              chakra: "1"
-            }
-          ], [
-            {
-              keycode: "51",
-              unicode: "\u0D15",
-              chakra: "1"
-            }, {
-              keycode: "52",
-              unicode: "\u0D16",
-              chakra: "1"
-            }, {
-              keycode: "53",
-              unicode: "\u0D17",
-              chakra: "1"
-            }, {
-              keycode: "54",
-              unicode: "\u0D18",
-              chakra: "1"
-            }, {
-              keycode: "55",
-              unicode: "\u0D19",
-              chakra: "1"
-            }
-          ], [
-            {
-              keycode: "56",
-              unicode: "\u0D1A",
-              chakra: "1"
-            }, {
-              keycode: "57",
-              unicode: "\u0D1B",
-              chakra: "1"
-            }, {
-              keycode: "58",
-              unicode: "\u0D1C",
-              chakra: "1"
-            }, {
-              keycode: "59",
-              unicode: "\u0D1D",
-              chakra: "1"
-            }, {
-              keycode: "60",
-              unicode: "\u0D1E",
-              chakra: "1"
-            }
-          ], [
-            {
-              keycode: "61",
-              unicode: "\u0D15",
-              chakra: "1"
-            }, {
-              keycode: "62",
-              unicode: "\u0D16",
-              chakra: "1"
-            }, {
-              keycode: "63",
-              unicode: "\u0D17",
-              chakra: "1"
-            }, {
-              keycode: "64",
-              unicode: "\u0D18",
-              chakra: "1"
-            }, {
-              keycode: "65",
-              unicode: "\u0D19",
-              chakra: "1"
-            }
-          ], [
-            {
-              keycode: "66",
-              unicode: "\u0D1A",
-              chakra: "1"
-            }, {
-              keycode: "67",
-              unicode: "\u0D1B",
-              chakra: "1"
-            }, {
-              keycode: "68",
-              unicode: "\u0D1C",
-              chakra: "1"
-            }, {}, {}
-          ]
-        ];
-        console.log($scope.onscreen);
+        $scope.onscreen = grouped;
+        console.log(grouped);
       });
       $scope.displaychakra = function(keycode, unicode, event) {
-        angular.element('#chakra').css('left', event.screenX - 70).css('top', event.screenY - 180).css('display', 'block');
-        angular.element('#innerchakra').css('position', 'absolute');
+        angular.element("#chakra").css("left", event.screenX - 70).css("top", event.screenY - 180).css("display", "block");
+        angular.element("#innerchakra").css("position", "absolute");
         $scope.currentkey = unicode;
       };
       $scope.hidechakra = function() {
-        angular.element('#chakra').css('display', 'none');
+        angular.element("#chakra").css("display", "none");
       };
       $scope.shifttable = function() {
         $scope.frame1 = !$scope.frame1;
       };
-      $scope.sharetext = function() {
+      return $scope.sharetext = function() {
         return function() {};
       };
     }
